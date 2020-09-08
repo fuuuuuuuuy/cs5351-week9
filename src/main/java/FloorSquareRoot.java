aa68fc39fe25b69298522a5118d39cf319f6f05c
Jankin Wei <weizhy3@hotmail.com>
2020-09-08 14:50:43 +0800

    Add files via upload

--- src/main/java/FloorSquareRoot.java
+++ src/main/java/FloorSquareRoot.java
@@ -4,7 +4,7 @@ public class FloorSquareRoot {
     public static int floorSqrt(int x) {
         if (x == 0 || x == 1) return x;
 
-        int start = 1, end = x, ans = 0;
+        int start = 2, end = x, ans = 0;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (mid * mid == x) return mid;
